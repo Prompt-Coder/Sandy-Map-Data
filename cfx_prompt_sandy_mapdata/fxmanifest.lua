eaa0e49986c00f434a5725a91306b1cf6fcee2dc
Prompt X <stanislav.semenov.2016@mail.ru>
2025-01-13 15:11:49 -0800

    Add map data for (Sandy Map Data) - Houses1

---
+++ cfx_prompt_sandy_mapdata/fxmanifest.lua
@@ -0,0 +1,19 @@
+fx_version "cerulean"
+games { 'gta5' }
+ 
+author 'Prompt Mods'
+description 'Houses1'
+version '1.0.0'
+ 
+this_is_a_map 'yes'
+ 
+escrow_ignore {
+    'stream/**'
+}
+ 
+-- scripts --
+lua54 'yes'
+ 
+server_scripts{
+    'sv_MapDataHandler.lua'
+}
