ab2f2b86fd4f7e1d968cc5773026f8b6986eaf47
Prompt X <stanislav.semenov.2016@mail.ru>
2025-01-31 11:46:53 -0800

    Add map data for (Sandy Map Data) - Cityhall+Marina+Boathouse+Sheriff+Hospital+Airfield+Firestation+Gnmotel

--- cfx_prompt_sandy_mapdata/fxmanifest.lua
+++ cfx_prompt_sandy_mapdata/fxmanifest.lua
@@ -1,19 +1,19 @@
 fx_version "cerulean"
 games { 'gta5' }
-
+ 
 author 'Prompt Mods'
-description 'SandyMapData----Houses1+Marina+Sheriff+Hospital+Airfield'
+description 'Cityhall+Marina+Boathouse+Sheriff+Hospital+Airfield+Firestation+Gnmotel'
 version '1.0.0'
-
+ 
 this_is_a_map 'yes'
-
+ 
 escrow_ignore {
     'stream/**'
 }
-
+ 
 -- scripts --
 lua54 'yes'
-
+ 
 server_scripts{
     'sv_MapDataHandler.lua'
 }
