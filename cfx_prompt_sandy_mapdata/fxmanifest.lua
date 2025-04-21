afeef2905284eb56e9c1774192f54c7231cff94e
Prompt-Coder <65853453+Prompt-Coder@users.noreply.github.com>
2025-04-20 21:32:38 -0700

    Add map data for (Sandy Map Data) - Church+Dealer+Marina+Boathouse

--- cfx_prompt_sandy_mapdata/fxmanifest.lua
+++ cfx_prompt_sandy_mapdata/fxmanifest.lua
@@ -1,19 +1,26 @@
 fx_version "cerulean"
 games { 'gta5' }
-
+ 
 author 'Prompt Mods'
-description 'SandyMapData----Houses1+Marina+Sheriff+Hospital+Airfield'
+description 'Church+Dealer+Marina+Boathouse'
 version '1.0.0'
-
+ 
 this_is_a_map 'yes'
-
+ 
 escrow_ignore {
     'stream/**'
 }
-
+ 
 -- scripts --
 lua54 'yes'
 
-server_scripts{
-    'sv_MapDataHandler.lua'
+
+client_scripts {
+    'client.js'
 }
+
+server_scripts{
+    'sv_MapDataHandler.lua',
+    'server.js'
+}
+
