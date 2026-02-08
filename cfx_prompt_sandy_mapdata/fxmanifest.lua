b770ac4083fb7af055a2685c42b6b01453018d6b
Vertex-Hub <vertexhub.fivem@gmail.com>
2026-02-08 16:09:47 +0000

    Update map data - Apts+Gym+Hornys+Bank+Market+Motel+Gas+Mechanic+Church+Dealer+Cityhall+Houses1+Marina+Boathouse+Sheriff+Hospital+Airfield+Firestation

---
+++ cfx_prompt_sandy_mapdata/fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version "cerulean"
+games { 'gta5' }
+
+author 'Prompt Mods'
+description 'Apts+Gym+Hornys+Bank+Market+Motel+Gas+Mechanic+Church+Dealer+Cityhall+Houses1+Marina+Boathouse+Sheriff+Hospital+Airfield+Firestation'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+escrow_ignore {
+    'stream/**'
+}
+
+-- scripts --
+lua54 'yes'
+
+
+client_scripts {
+    'client.js'
+}
+
+server_scripts{
+    'sv_MapDataHandler.lua',
+    'server.js'
+}
