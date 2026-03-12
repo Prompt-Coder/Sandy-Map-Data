a285420ae4454751efcda796e46c38d81d2467a4
Vertex-Hub <vertexhub.fivem@gmail.com>
2026-03-12 15:55:30 +0000

    Update map data - Downtown+Joshua+Uni+Corner+Apts+Hornys+Bank+Market+Motel+Church+Houses1

---
+++ cfx_prompt_sandy_mapdata/fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version "cerulean"
+games { 'gta5' }
+
+author 'Prompt Mods'
+description 'Downtown+Joshua+Uni+Corner+Apts+Hornys+Bank+Market+Motel+Church+Houses1'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+escrow_ignore {
+    'stream/**'
+}
+
+-- scripts --
+lua54 'yes'
+
+
+client_scripts {
+    'client.js'
+}
+
+server_scripts{
+    'sv_MapDataHandler.lua',
+    'server.js'
+}
