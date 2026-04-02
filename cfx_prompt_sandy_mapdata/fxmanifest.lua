975ac32e97e0fb0b288b075aa51a5c8cf9065bfc
Vertex-Hub <vertexhub.fivem@gmail.com>
2026-04-02 21:52:58 +0000

    Update map data - Uni+Apts+Gym+Market+Motel+Church+Dealer+Houses1+Marina+Boathouse+Hospital+Airfield

---
+++ cfx_prompt_sandy_mapdata/fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version "cerulean"
+games { 'gta5' }
+
+author 'Prompt Mods'
+description 'Uni+Apts+Gym+Market+Motel+Church+Dealer+Houses1+Marina+Boathouse+Hospital+Airfield'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+escrow_ignore {
+    'stream/**'
+}
+
+-- scripts --
+lua54 'yes'
+
+
+client_scripts {
+    'client.js'
+}
+
+server_scripts{
+    'sv_MapDataHandler.lua',
+    'server.js'
+}
