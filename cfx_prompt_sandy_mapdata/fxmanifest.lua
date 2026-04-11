13f56d8a17e20382ae506096091ef6728d4b763c
Vertex-Hub <vertexhub.fivem@gmail.com>
2026-04-11 01:21:57 +0000

    Update map data - Downtown+Hospital2+Joshua+Uni+Corner+Apts+Gym+Hornys+Bank+Market+Motel+Gas+Mechanic+Church+Dealer+Cityhall+Houses1+Trainstation+Marina+Boathouse+Sheriff+Firestation

---
+++ cfx_prompt_sandy_mapdata/fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version "cerulean"
+games { 'gta5' }
+
+author 'Prompt Mods'
+description 'Downtown+Hospital2+Joshua+Uni+Corner+Apts+Gym+Hornys+Bank+Market+Motel+Gas+Mechanic+Church+Dealer+Cityhall+Houses1+Trainstation+Marina+Boathouse+Sheriff+Firestation'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+escrow_ignore {
+    'stream/**'
+}
+
+-- scripts --
+lua54 'yes'
+
+
+client_scripts {
+    'client.js'
+}
+
+server_scripts{
+    'sv_MapDataHandler.lua',
+    'server.js'
+}
