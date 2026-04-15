ed9d8d622e82d138c5b1e8bd2dd2e61a8267a769
Vertex-Hub <vertexhub.fivem@gmail.com>
2026-04-15 14:51:26 +0000

    Update map data - Hospital2+Gym+Hornys+Market+Gas+Mechanic+Houses1+Marina+Boathouse+Sheriff+Airfield

---
+++ cfx_prompt_sandy_mapdata/fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version "cerulean"
+games { 'gta5' }
+
+author 'Prompt Mods'
+description 'Hospital2+Gym+Hornys+Market+Gas+Mechanic+Houses1+Marina+Boathouse+Sheriff+Airfield'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+escrow_ignore {
+    'stream/**'
+}
+
+-- scripts --
+lua54 'yes'
+
+
+client_scripts {
+    'client.js'
+}
+
+server_scripts{
+    'sv_MapDataHandler.lua',
+    'server.js'
+}
