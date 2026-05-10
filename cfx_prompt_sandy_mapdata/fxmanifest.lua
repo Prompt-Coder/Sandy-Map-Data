2d22a3eb3412febf6e8f75ff0594def4d29a68fc
Vertex-Hub <vertexhub.fivem@gmail.com>
2026-05-10 05:34:43 +0000

    Update map data - Roads+Downtown+Firestation2+Uni+Corner+Apts+Hornys+Market+Motel+Mechanic+Dealer+Cityhall+Houses1+Trainstation+Sheriff

---
+++ cfx_prompt_sandy_mapdata/fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version "cerulean"
+games { 'gta5' }
+
+author 'Prompt Mods'
+description 'Roads+Downtown+Firestation2+Uni+Corner+Apts+Hornys+Market+Motel+Mechanic+Dealer+Cityhall+Houses1+Trainstation+Sheriff'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+escrow_ignore {
+    'stream/**'
+}
+
+-- scripts --
+lua54 'yes'
+
+
+client_scripts {
+    'client.js'
+}
+
+server_scripts{
+    'sv_MapDataHandler.lua',
+    'server.js'
+}
