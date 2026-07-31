97d56d7724261ba398a05ecfc74ad956a12e1030
Vertex-Hub <vertexhub.fivem@gmail.com>
2026-07-31 08:35:42 +0000

    Update map data - Roads+Apts+Gym+Market+Motel+Gas+Dealer+Houses1+Trainstation+Boathouse+Airfield

---
+++ cfx_prompt_sandy_mapdata/fxmanifest.lua
@@ -0,0 +1,25 @@
+fx_version "cerulean"
+games { 'gta5' }
+
+author 'Prompt Mods'
+description 'Roads+Apts+Gym+Market+Motel+Gas+Dealer+Houses1+Trainstation+Boathouse+Airfield'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+escrow_ignore {
+    'stream/**'
+}
+
+-- scripts --
+lua54 'yes'
+
+
+client_scripts {
+    'client.js'
+}
+
+server_scripts{
+    'sv_MapDataHandler.lua',
+    'server.js'
+}
