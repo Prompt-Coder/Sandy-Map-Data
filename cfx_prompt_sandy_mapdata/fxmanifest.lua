4933c7e1bb70752205b35ca16c7d6abdcf97dbac
Vertex-Hub <vertexhub.fivem@gmail.com>
2026-08-29 11:28:34 +0000

    Update map data - Roads+Downtown+Flywheels+Joshua+Uni+Corner+Apts+Gym+Hornys+Bank+Market+Motel+Gas+Mechanic+Church+Dealer+Cityhall+Houses1+Trainstation+Marina+Boathouse+Hospital+Airfield+Firestation

---
+++ cfx_prompt_sandy_mapdata/fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version "cerulean"
+games { 'gta5' }
+
+author 'Prompt Mods'
+description 'Roads+Downtown+Flywheels+Joshua+Uni+Corner+Apts+Gym+Hornys+Bank+Market+Motel+Gas+Mechanic+Church+Dealer+Cityhall+Houses1+Trainstation+Marina+Boathouse+Hospital+Airfield+Firestation'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+escrow_ignore {
+    'stream/**'
+}
+
+-- scripts --
+lua54 'yes'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/prompt_props/prompt_sandy_props.ytyp'
+
+client_scripts {
+    'client.js'
+}
+
+server_scripts{
+    'sv_MapDataHandler.lua',
+    'server.js'
+}
