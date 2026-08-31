24ac4d13c6e90ace45372a1af81b465772ad1987
Vertex-Hub <vertexhub.fivem@gmail.com>
2026-08-31 03:02:46 +0000

    Update map data - Gym+Trainstation+Marina+Airfield

---
+++ cfx_prompt_sandy_mapdata/fxmanifest.lua
@@ -0,0 +1,26 @@
+fx_version "cerulean"
+games { 'gta5' }
+
+author 'Prompt Mods'
+description 'Gym+Trainstation+Marina+Airfield'
+version '1.0.0'
+
+this_is_a_map 'yes'
+
+escrow_ignore {
+    'stream/**'
+}
+
+-- scripts --
+lua54 'yes'
+
+data_file 'DLC_ITYP_REQUEST' 'stream/prompt_props/prompt_sandy_props.ytyp'
+
+client_scripts {
+    'client.js'
+}
+
+server_scripts{
+    'sv_MapDataHandler.lua',
+    'server.js'
+}
