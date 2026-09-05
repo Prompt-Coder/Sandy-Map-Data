fdcbd30092e0e6b864e0e0d3a9e458cfae2544d1
Vertex-Hub <vertexhub.fivem@gmail.com>
2026-09-05 19:07:32 +0000

    Update map data - Roads+Downtown+Uni+Corner+Gym+Motel+Gas+Church+Cityhall+Houses1+Trainstation+Marina+Hospital+Airfield+Firestation

---
+++ cfx_prompt_sandy_mapdata/sv_MapDataHandler.lua
@@ -0,0 +1,46 @@
+local maps = {
+	"prompt_sandy_roads",
+	"prompt_sandy_downtown",
+	"prompt_sandy_university",
+	"prompt_sandy_corner",
+	"prompt_sandy_gym",
+	"prompt_sandy_motel",
+	"prompt_sandy_gasstation_carwash",
+	"prompt_sandy_church",
+	"prompt_sandy_cityhall",
+	"sandy_shores_houses_pt1",
+	"prompt_sandy_train_station",
+	"prompt_sandy_beaches",
+	"prompt_sandy_hospital",
+	"prompt_sandy_airfield",
+	"cfx_prompt_sandy_shores_fire_department"
+}
+
+local events = {}
+
+-- Sandy mapdata exists event
+RegisterNetEvent("prompt:mapdata_exists", function(cb)
+  cb(true)
+end)
+
+-- Sandy mapdata list event
+RegisterNetEvent("prompt:mapdata_sendList", function(returnevent)
+  TriggerEvent(returnevent, maps)
+end)
+
+-- Legacy mapdata exists event
+RegisterNetEvent("lyn-mapdata:exists", function(cb)
+  cb(true)
+end)
+
+-- Legacy support for individual map checks
+for i = 1, #maps do
+  local eventName = maps[i] .. ":mapDataExists"
+  if Debug == true then
+    print("Creating event: ", eventName)
+  end
+  local event = RegisterNetEvent(eventName, function(cb)
+    cb(true)
+  end)
+  table.insert(events, event)
+end
